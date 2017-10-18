e2fb9611e0f525450f60560cd21a8c4bf3918e52
Kiril Kolev <minikibork4@abv.bg>
2017-10-18 19:20:41 +0200

    End of day fixes

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -7,22 +7,53 @@ public class EventManager : MonoBehaviour, IPointerDownHandler
 
     public delegate void ClickAction();
     public static event ClickAction OnClick;
+    public List<int> selectedCards = new List<int>();
+   // public int[] selected = new int[]
     PointerEventData cardIndex;
     GameObject cardy;
     CardInformation cards;
+    public static int n=0;
     public int cardIndexCopy;
     public virtual void OnPointerDown(PointerEventData eventData)
     {
-        cards = GetComponent<CardInformation>();
-        cardIndexCopy = cards.cardIndex;
-       // cardIndex = eventData.rawPointerPress;
-        cardy = eventData.selectedObject.GetComponent<GameObject>();
-        Debug.Log(cardIndexCopy);
+        
+         cards = GetComponent<CardInformation>();
+         cardIndexCopy = cards.cardIndex;
+         n++; 
+         selectedCards.Insert(0, cardIndexCopy);
+         // cardIndex = eventData.rawPointerPress;
+         Debug.Log(selectedCards[0]);
+         Debug.Log(n);
+         //Debug.Log(selectedCards[1]);
+         //Debug.Log(cardIndexCopy);
+         Debug.Log(selectedCards.Count);
+         
+         
         if (OnClick != null)
         {
             OnClick();
         }
-       
+        Debug.Log("§§§§§§§§§§§§§§§§§§§§§§§§§§§");
     }
 
 }
+
+
+
+/*
+         cards = GetComponent<CardInformation>();
+         cardIndexCopy = cards.cardIndex;
+         n++; 
+         selectedCards.Insert(0, cardIndexCopy);
+         // cardIndex = eventData.rawPointerPress;
+         Debug.Log(selectedCards[0]);
+         Debug.Log(n);
+         //Debug.Log(selectedCards[1]);
+         //Debug.Log(cardIndexCopy);
+         Debug.Log(selectedCards.Count);
+
+         if (OnClick != null)
+         {
+             OnClick();
+         }
+         Debug.Log("§§§§§§§§§§§§§§§§§§§§§§§§§§§");*/
